79342151ef55ce6f3c9999bb8b8c9bd77fd345bc
rfs <remo@mountain-yard.ch>
2023-05-24 17:20:24 +0200

    feat: implement controller to extract email address from twitch info page

---
+++ data-extraction-twitch/src/main/java/ch/resrc/gamerm/dataextractiontwitch/ScraperController.java
@@ -0,0 +1,22 @@
+package ch.resrc.gamerm.dataextractiontwitch;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ScraperController {
+    private final ScraperService scraperService;
+
+    public ScraperController(ScraperService twitchService) {
+        this.scraperService = twitchService;
+    }
+
+    @GetMapping("/twitch/{streamerId}")
+    public List<String> getEmail(@PathVariable String streamerId) {
+        System.out.println("streamerId: " + streamerId);
+        return scraperService.extractEmailFromTwitchInfoPage(streamerId);
+    }
+}
