e7545a2038a3eaa70d3d59575176cda8b70df218
rfs <remo@mountain-yard.ch>
2023-05-24 12:43:51 +0200

    deps: initialize spring boot app

---
+++ data-extraction-twitch/src/main/java/ch/resrc/gamerm/dataextractiontwitch/DataExtractionTwitchApplication.java
@@ -0,0 +1,13 @@
+package ch.resrc.gamerm.dataextractiontwitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataExtractionTwitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataExtractionTwitchApplication.class, args);
+	}
+
+}
---
+++ data-extraction-twitch/src/test/java/ch/resrc/gamerm/dataextractiontwitch/DataExtractionTwitchApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.resrc.gamerm.dataextractiontwitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataExtractionTwitchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
